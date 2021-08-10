96b66b259b25b98e2113a79f0ff2a124c4155c4f
Stephen Wong <swong4009@gmail.com>
2021-08-10 16:28:52 -0400

    updated wording

--- Develop/index.js
+++ Develop/index.js
@@ -29,7 +29,7 @@ const questions = [{
     {
         type: 'input',
         name: 'howToContribute',
-        message: 'Enter the contribution guidelines',
+        message: 'How to contribute',
     },
     {
         type: 'input',
